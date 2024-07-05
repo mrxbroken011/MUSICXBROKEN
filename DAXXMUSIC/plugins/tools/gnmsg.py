7af902a2aeb88dfdbf56a35361ec328c1778ce08
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-07-05 17:27:52 +0530

    Update gnmsg.py

--- DAXXMUSIC/plugins/tools/gnmsg.py
+++ DAXXMUSIC/plugins/tools/gnmsg.py
@@ -4,18 +4,18 @@ import random
 from DAXXMUSIC import app
 
 
-@app.on_message(filters.command(["gn","n","oodnight","ood Night","ood night"], prefixes=["/","g","G"]))
+@app.on_message(filters.command(["gn","n","oodnight","ood Night","ood night"], prefixes=["","g","G"]))
 def goodnight_command_handler(_, message):
     sender = message.from_user.mention
     send_sticker = random.choice([True, False])
     if send_sticker:
         sticker_id = get_random_sticker()
         app.send_sticker(message.chat.id, sticker_id)
-        message.reply_text(f"**Goodnight, {sender}! Sleep tight. 🌙**")
+        message.reply_text(f"**Goodnight, {sender}! Sleep well. 🌙**")
     else:
         emoji = get_random_emoji()
         app.send_message(message.chat.id, emoji)
-        message.reply_text(f"**Goodnight, {sender}! Sleep tight. {emoji}**")
+        message.reply_text(f"**Goodnight, {sender}! Sweet dreams. {emoji}**")
 
 
 def get_random_sticker():
@@ -34,5 +34,6 @@ def get_random_emoji():
         "😴",
         "😪",
         "💤",
+        "🥱", 
     ]
     return random.choice(emojis)
